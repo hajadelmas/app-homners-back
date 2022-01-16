3fbbbd79375d8aea51ca71222b2839ae7e0a28a4
hajadelmas <52510008+hajadelmas@users.noreply.github.com>
2022-01-16 15:47:29 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config()
+const mongoose = require("mongoose");
+
+const db = require("./app/models");
+const Role = db.role;
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+
+db.mongoose
+  .connect(process.env.MONGODB_URI || `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-ybnth.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+const app = express();
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+  
+
+function initial() {
+Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+    new Role({
+        name: "user"
+    }).save(err => {
+        if (err) {
+        console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+    });
+
+    new Role({
+        name: "moderator"
+    }).save(err => {
+        if (err) {
+        console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+    });
+
+    new Role({
+        name: "admin"
+    }).save(err => {
+        if (err) {
+        console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+    });
+    }
+});
+}
+
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
